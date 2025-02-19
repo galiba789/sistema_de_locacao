a0733b20f8a06f95340db80d0db4fffb23c2c0ae
galiba <gabriel.liberato109@gmail.com>
2025-02-19 13:50:52 -0300

    Restart

---
+++ app/Views/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Playcom</title>
+    <link rel="stylesheet" href="<?=PL_BASE_DIST.'/css/style.css'?>">
+    <link rel="stylesheet" href="<?=PL_BASE_DIST.'/css/bootstrap.min.css'?>">
+</head>
+<body>
+<div class="container"></div>
+
+
+<!-- JS -->
+<script src="<?=PL_BASE_DIST.'/js/bootstrap.bundle.min.js'?>"></script>
+</body>
+</html>
